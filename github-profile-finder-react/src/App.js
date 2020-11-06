b209235da5fd5bab10e5d57ef747f48807b9fbbe
Sohee KWON <soheekwon@naver.com>
2020-11-06 19:14:49 +0900

    connect github API

--- github-profile-finder-react/src/App.js
+++ github-profile-finder-react/src/App.js
@@ -1,9 +1,29 @@
+import React from 'react';
 import './App.css';
+import SearchInput from './components/SearchInput.js';
+import SearchResult from './components/SearchResult.js'
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: "https://api.github.com/users/"
+});
 
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  const getUserAPI = async (username) => {
+    const data = await client.get(username);
+    setUser(data.data);
+  };
+
+  React.useEffect( () => {
+    getUserAPI("Sohee-K")
+  }, []);
+
   return (
     <>
-      <h1>React</h1>
+      <SearchInput />
+      <SearchResult />
     </>
   );
 }
